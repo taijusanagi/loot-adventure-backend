a77e1cff78eca808200096fd70935ca6139acddd
admin02-at-594175341170 <70833127+atakedemo@users.noreply.github.com>
2024-04-21 13:22:18 +0900

    add error-logic in seizureEquipment api

--- 30_backend/01_AWS/src/bin-seizure-equipment.ts
+++ 30_backend/01_AWS/src/bin-seizure-equipment.ts
@@ -48,62 +48,75 @@ export const handler = async (event: EventParams) => {
             })
         );
     } catch (error) {
-        throw error;
-    }
-    const secret = JSON.parse(response.SecretString as string);
-
-    // Execute Tx(TransferFrom EequipmentNFT from player)
-    const publicClient = createPublicClient({
-        chain: mchVerceTestnet,
-        transport: http(RPC_URL.MCHVERCE_TESTNET),
-    });
-    
-    const keyIndex = Math.floor(Math.random() * (11))
-    const account = privateKeyToAccount(secret[keyIndex]);
-    // const account = privateKeyToAccount(secret[0]);
-    const wallet = createWalletClient({
-        account,
-        chain: mchVerceTestnet,
-        transport: http(RPC_URL.MCHVERCE_TESTNET),
-    });
-
-    console.log('Soul Controler is: ', ADDRESS_SOUL_CONTROLER);
-    console.log('TBA is : ', event.userId)
-    const { request } = await publicClient.simulateContract({
-        address: ADDRESS_SOUL_CONTROLER,
-        abi: soulControlerAbi,
-        functionName: 'seizureEquipment',
-        account,
-        args: [event.userId],
-    });
-    const txHash = await wallet.writeContract(request);
-    console.log('transaction_hash', txHash);
-    const txReceipt = await publicClient.waitForTransactionReceipt( 
-        { hash: txHash }
-    )
-    console.log(txReceipt);
-    if(txReceipt['logs'].length > 2) { 
-        const topics = decodeEventLog({
-            abi: soulControlerAbi,
-            data: txReceipt['logs'][2]['data'] ,
-            topics: txReceipt['logs'][2]['topics']
+        console.log('Error: in get secretKey', error);
+        return JSON.stringify({
+            "tokenId": 0,
+            "type": 99
         })
-        console.log(topics);
-        const args = topics['args'] as EventSeizureEquipment;
-        if(typeof(args["tokenId"])!='undefined'){
-            console.log('tokenId; ', args["tokenId"].toString());
-            console.log('tokenType; ', args["tokenType"].toString());
-            return JSON.stringify({
-                "tokenId": parseInt(args["tokenId"].toString()),
-                "type": parseInt(args["tokenType"].toString())
+    }
+    
+    try {
+        const secret = JSON.parse(response.SecretString as string);
+
+        // Execute Tx(TransferFrom EequipmentNFT from player)
+        const publicClient = createPublicClient({
+            chain: mchVerceTestnet,
+            transport: http(RPC_URL.MCHVERCE_TESTNET),
+        });
+        
+        const keyIndex = Math.floor(Math.random() * (11))
+        const account = privateKeyToAccount(secret[keyIndex]);
+        // const account = privateKeyToAccount(secret[0]);
+        const wallet = createWalletClient({
+            account,
+            chain: mchVerceTestnet,
+            transport: http(RPC_URL.MCHVERCE_TESTNET),
+        });
+
+        console.log('Soul Controler is: ', ADDRESS_SOUL_CONTROLER);
+        console.log('TBA is : ', event.userId)
+        const { request } = await publicClient.simulateContract({
+            address: ADDRESS_SOUL_CONTROLER,
+            abi: soulControlerAbi,
+            functionName: 'seizureEquipment',
+            account,
+            args: [event.userId],
+        });
+        const txHash = await wallet.writeContract(request);
+        console.log('transaction_hash', txHash);
+        const txReceipt = await publicClient.waitForTransactionReceipt( 
+            { hash: txHash }
+        )
+        console.log(txReceipt);
+        if(txReceipt['logs'].length > 2) { 
+            const topics = decodeEventLog({
+                abi: soulControlerAbi,
+                data: txReceipt['logs'][2]['data'] ,
+                topics: txReceipt['logs'][2]['topics']
             })
+            console.log(topics);
+            const args = topics['args'] as EventSeizureEquipment;
+            if(typeof(args["tokenId"])!='undefined'){
+                console.log('tokenId; ', args["tokenId"].toString());
+                console.log('tokenType; ', args["tokenType"].toString());
+                return JSON.stringify({
+                    "tokenId": parseInt(args["tokenId"].toString()),
+                    "type": parseInt(args["tokenType"].toString())
+                })
+            } else {
+                return JSON.stringify({
+                    "tokenId": 0,
+                    "type": 99
+                })
+            }
         } else {
             return JSON.stringify({
                 "tokenId": 0,
                 "type": 99
             })
         }
-    } else {
+    } catch (error) {
+        console.log('Error: in execute_tx', error);
         return JSON.stringify({
             "tokenId": 0,
             "type": 99
